1d212e02ed3b5e83a83b223921e7ef74426f5660
Yota Toyama <raviqqe@gmail.com>
2022-09-27 18:41:33 -0700

    Fix /

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as core from "@actions/core";
 
 for (const path of ["bin", "sbin"]) {
-  core.addPath("/home/linuxbrew/.linuxbrew" + path);
+  core.addPath("/home/linuxbrew/.linuxbrew/" + path);
 }
