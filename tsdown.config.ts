210bc3f6e9faac039f238169fe34b6d973333fcc
Yota Toyama <raviqqe@gmail.com>
2026-01-30 07:57:29 -0800

    fix

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  noExternal: ["@actions/*"],
   platform: "browser",
-  skipNodeModulesBundle: true,
+  skipNodeModulesBundle: false,
 });
